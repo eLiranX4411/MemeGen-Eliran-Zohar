7f430c353525afce57342678dbabfefb2e0a0344
eLiranX4411 <solidityart@gmail.com>
2024-08-08 21:05:32 +0300

    Fix save screen, remove some logs

--- js/controllers/savedController.js
+++ js/controllers/savedController.js
@@ -16,22 +16,3 @@ function renderSavedMemes() {
 
   elSavedImgsContainer.innerHTML = imgsHtml
 }
-
-// function loadSavedMemes() {
-//   const savedMemes = loadFromStorage(MEME_STORAGE_KEY) || []
-//   const elSavedImgs = document.querySelector('.saved-imgs')
-
-//   const strHtml = savedMemes
-//     .map((meme, idx) => {
-//       return `<img src="${meme.imgData}" alt="Meme ${idx}" onclick="onSelectSavedMeme(${idx})" />`
-//     })
-//     .join('')
-
-//   elSavedImgs.innerHTML = strHtml
-// }
-
-// function onSelectSavedMeme(idx) {
-//   const savedMemes = loadFromStorage(MEME_STORAGE_KEY) || []
-//   gMeme = savedMemes[idx].memeData
-//   renderMeme()
-// }
--- js/services/savedService.js
+++ js/services/savedService.js
@@ -5,8 +5,6 @@ function saveCanvasMemeToStorage() {
 
   let savedMemes = loadFromStorage(MEME_STORAGE_KEY)
 
-  console.log('Loaded savedMemes:', savedMemes)
-
   if (!Array.isArray(savedMemes)) {
     savedMemes = []
   }
@@ -16,34 +14,7 @@ function saveCanvasMemeToStorage() {
     memeData: gMeme,
   }
 
-  console.log('Before saving, savedMemes:', savedMemes)
-
   savedMemes.push(memeToSave)
-
-  console.log('After saving, savedMemes:', savedMemes)
 
   saveToStorage(MEME_STORAGE_KEY, savedMemes)
 }
-
-// function saveCanvasMemeToStorage() {
-//     const imgData = gElCanvas.toDataURL('image/png')
-
-//     let savedMemes = loadFromStorage(MEME_STORAGE_KEY) || []
-//     console.log('Loaded savedMemes:', savedMemes)
-
-//     if (!Array.isArray(savedMemes)) {
-//       savedMemes = []
-//     }
-//     const memeToSave = {
-//       imgData: imgData,
-//       memeData: gMeme,
-//     }
-
-//     console.log('Before saving, savedMemes:', savedMemes)
-
-//     savedMemes.push(memeToSave)
-
-//     console.log('After saving, savedMemes:', savedMemes)
-
-//     saveToStorage(MEME_STORAGE_KEY, savedMemes)
-//   }
